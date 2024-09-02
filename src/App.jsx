c961d0bec5276112bc406192bef8d1900aa43806
Vishal <vishalsharma05052002@gmail.com>
2024-09-02 19:54:55 +0530

    removed proxy

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-   import { setContext } from '@apollo/client/link/context';
-  import HashPulse from './pages/HashPulse';
-   const httpLink = createHttpLink({
-     uri: '/graphql', // This will be proxied to  https://gql.hashnode.com
-   });
-   const API_TOKEN=import.meta.env.VITE_API_TOKEN;
-   const authLink = setContext((_, { headers }) => {
-     return {
-       headers: {
-         ...headers,
-         authorization: `Bearer ${API_TOKEN}`,
-       }
-     }
-   });
+import { setContext } from '@apollo/client/link/context';
+import HashPulse from './pages/HashPulse';
 
-   const client = new ApolloClient({
-     link: authLink.concat(httpLink),
-     cache: new InMemoryCache()
-   });
+const httpLink = createHttpLink({
+  uri: 'https://gql.hashnode.com/graphql',
+});
+
+const API_TOKEN = import.meta.env.VITE_API_TOKEN;
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${API_TOKEN}`,
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
 function App() {
   return (
     <ApolloProvider client={client}>
       <HashPulse/>
     </ApolloProvider>
   );
-
 }
-
 
 export default App;
